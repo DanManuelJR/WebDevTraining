8abe6d4a2c6af94b7c38d09f134a117099145e3f
DanManuelJR <danmanuel75@gmail.com>
2022-04-07 21:41:43 +0800

    methods.java

---
+++ methods.java
@@ -0,0 +1,16 @@
+ class methods {
+
+    public static void main(String[] args){
+        displayName("John Doe");
+}
+
+public static void displayName(String name){
+    System.out.println("My name is "  + name);
+
+ }
+
+ public static void addTwoNumbers(int num1, int num2){
+     System.out.println(num1 + num2);
+
+ }
+}
